f69043e258a3794cdc73d6d1d230da1d0b9a3c16
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2022-05-05 11:33:50 -0300

    Update datapane_report.py

--- src/datapane_report.py
+++ src/datapane_report.py
@@ -9,7 +9,7 @@ requirement_list = ["python-dotenv==0.20.0",
 "yellowbrick==1.4",
 "scikit-learn==1.0.2",
 "plotly==5.7.0",
-"unidecodedata2==14.0.0"]
+"unidecodedata2"]
 
 install_packages(requirement_list)
 
